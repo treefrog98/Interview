5231b8c523e3066faf355cccf3cca029b1452eb7
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-30 22:33:57 -0500

    house rob

---
+++ LeetCode/HouseRobber.java
@@ -0,0 +1,19 @@
+public class HouseRobber {
+
+  public static void main(String[] args) {
+
+
+  }
+  public static int rob(int[] nums) {
+       int even = 0;
+       int odd = 0;
+       for (int i = 0; i < nums.length; i++) {
+           if (i % 2 == 0) {
+               even = Math.max(even + nums[i], odd);
+           } else {
+               odd = Math.max(odd + nums[i], even);
+           }
+       }
+       return Math.max(even, odd);
+  }
+}
