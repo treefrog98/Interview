28e280aa062bfc8baf0c1ec53eb29a2c2de3cc31
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-30 20:53:38 -0500

    2 sum sorted

---
+++ LeetCode/TwoSumSorted.java
@@ -0,0 +1,29 @@
+public class TwoSumSorted {
+
+  public static void main(String[] args) {
+
+
+
+
+  }
+  public int[] twoSum(int[] numbers, int target) {
+        int [] arr = new int[2];
+        // first find the index it is less than
+        int leftBound = 0;
+        int rightBound = numbers.length-1;
+
+        while(numbers[leftBound] + numbers[rightBound] != target && leftBound < rightBound){
+            if(numbers[rightBound] + numbers[leftBound] < target){
+                leftBound++;
+            }
+            else{
+                rightBound--;
+            }
+        }
+         arr[0] = leftBound+1;
+         arr[1] = rightBound+1;
+        return arr;
+
+    }
+
+}
