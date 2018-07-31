3045597092ffd142d8e429bbbdaf04820c843c17
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-31 12:44:26 -0500

    linked list cycle

---
+++ LeetCode/LinkedListCycle.cpp
@@ -0,0 +1,28 @@
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+}
+
+class LinkedListCycle {
+public:
+  bool hasCycle(ListNode *head) {
+      if(head == NULL || head->next == NULL) {
+          return false;
+      }
+
+      ListNode* first = head;
+      ListNode* second = head;
+      while(first != NULL && second != NULL && second->next != NULL) {
+          first = first->next;
+          second = second->next->next;
+          if(first == second) {
+              return true;
+          }
+      }
+      return false;
+
+      // 10 -> 11 -> 12 -> 13 -> (11)
+
+  }
+};
