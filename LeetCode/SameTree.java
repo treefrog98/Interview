802c8b7906d0a6faaf9013af5907879c7444af1a
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-30 19:50:23 -0500

    same tree

---
+++ LeetCode/SameTree.java
@@ -0,0 +1,25 @@
+private class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) {val = x ;}
+}
+
+public class SameTree {
+
+  public static void main(String[] args) {
+
+
+
+  }
+
+  public static boolean sameTree(TreeNode p, TreeNode q) {
+     if(p == null && q == null) return true;
+     if(p == null || q == null) return false;
+     if(p.val == q.val)
+         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+     return false;
+  }
+
+
+}
