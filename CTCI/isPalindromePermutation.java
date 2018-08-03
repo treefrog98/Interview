b9c67930b3f7139789aedc50679aea28d5e8e69d
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-08-02 22:31:23 -0500

    is palindrome permutation

--- CTCI/isPalindromePermutation.java
+++ CTCI/isPalindromePermutation.java
@@ -7,7 +7,7 @@ public class isPalindromePermutation {
 	}
 
 	public static int[] count_Occurrences(String s) {
-		String new_string = s.toLowerCase();
+		  String new_string = s.toLowerCase();
 	    int[] occurrences = new int[128];
 	    for(int i = 0; i < new_string.length(); i++) {
 	      char c = new_string.charAt(i);
@@ -16,23 +16,23 @@ public class isPalindromePermutation {
 	    return occurrences;
 	}
 	public static boolean isPalindromePermutation(int[] arr) {
-		boolean odd_occurrences = false;
-		for(int i = 97; i < 123; i++) {
-			if(arr[i] % 2 != 0) {
-				if(odd_occurrences) {
-					return false;
-				}
-				odd_occurrences = true;
-			}
-		}
-		for(int i = 48; i < 58; i++) {
-			if(arr[i] % 2 != 0) {
-				if(odd_occurrences) {
-					return false;
-				}
-				odd_occurrences = true;
-			}
-		}
-		return true;
-	}
+  		boolean odd_occurrences = false;
+  		for(int i = 97; i < 123; i++) {
+  			if(arr[i] % 2 != 0) {
+  				if(odd_occurrences) {
+  					return false;
+  				}
+  				odd_occurrences = true;
+  			}
+  		}
+  		for(int i = 48; i < 58; i++) {
+  			if(arr[i] % 2 != 0) {
+  				if(odd_occurrences) {
+  					return false;
+  				}
+  				odd_occurrences = true;
+  			}
+  		}
+  		return true;
+  	}
 }
